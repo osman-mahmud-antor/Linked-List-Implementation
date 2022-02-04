299fa348213697ce5fb60559e4288c6a29ebc63b
Antor Mahmud <54511544+osman-mahmud-antor@users.noreply.github.com>
2022-02-04 03:13:49 -1200

    Create insert_at_last.c

---
+++ insert_at_last.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct node Node;
+struct node{
+    int data;
+    Node *next;
+};
+
+Node *create_node(int item, Node *next){
+    Node *new_node = (Node*)malloc(sizeof(Node));
+    if(new_node==NULL){
+        printf("Error!Couldn't create a new node\n");
+        exit(1);
+    }
+    new_node->data=item;
+    new_node->next=next;
+
+    return new_node;
+}
+
+Node *prepend(Node *head,int item){
+    Node *new_node = create_node(item,head);
+    return new_node;
+}
+
+Node *append(Node *head,int item){
+    Node *new_node = create_node(item,NULL);
+    if(head==NULL){
+        return new_node;
+    }
+    Node *current_node = head;
+    while(current_node->next!=NULL){
+        current_node=current_node->next;
+    }
+    current_node->next=new_node;
+
+    return head;
+}
+
+void print_linked_list(Node *head){
+    Node *current_node = head;
+    while(current_node!=NULL){
+        printf("%d ",current_node->data);
+        current_node=current_node->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    Node *n1,*n2,*head,*n3;
+    n1 = create_node(10,NULL);
+    head = n1;
+    print_linked_list(head);
+    head = prepend(head,20);
+    print_linked_list(head);
+    head = append(head,30);
+    print_linked_list(head);
+
+   return 0;
+}
